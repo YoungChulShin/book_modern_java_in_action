e7294609720bd68b6c4a8104fc41a590a089e762
YoungChulShin <go1323@gmail.com>
2021-08-10 23:13:28 +0900

    sort with lambda

--- modern-java/ch2/src/main/java/ch2/caller/GenericPredicateCaller.java
+++ modern-java/ch2/src/main/java/ch2/caller/GenericPredicateCaller.java
@@ -4,6 +4,7 @@ import ch2.filter.GenericFilter;
 import ch2.model.Apple;
 import ch2.model.Color;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class GenericPredicateCaller {
@@ -19,5 +20,26 @@ public class GenericPredicateCaller {
     List<Integer> numbers = new ArrayList<>();
 
     List<Integer> result = GenericFilter.filter(numbers, (Integer i) -> i % 2 == 0);
+  }
+
+  public void sortApples() {
+    List<Apple> apples = new ArrayList<>();
+
+    apples.sort(new Comparator<Apple>() {
+      @Override
+      public int compare(Apple o1, Apple o2) {
+        return Integer.compare(o1.getWeight(), o2.getWeight());
+      }
+    });
+  }
+
+  public void sortApplesWithLamba() {
+    List<Apple> apples = new ArrayList<>();
+
+    apples.sort((Apple a1, Apple a2) -> Integer.compare(a1.getWeight(), a2.getWeight()));
+  }
+
+  public void runnableThread() {
+    Thread thread = new Thread(() -> System.out.println("Hello"));
   }
 }
