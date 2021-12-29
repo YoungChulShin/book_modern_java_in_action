65d4981467df15c287d12b4bbefbf40c70a390d4
YoungChulShin <go1323@gmail.com>
2021-12-30 06:28:15 +0900

    4장 기본 코드 추가

---
+++ modern-java/src/main/java/modern/java/ch4/Ch4Helper.java
@@ -0,0 +1,23 @@
+package modern.java.ch4;
+
+import java.util.Arrays;
+import java.util.List;
+import modern.java.ch4.Dish.Type;
+
+public class Ch4Helper {
+
+  public static List<Dish> getMenu() {
+    return Arrays.asList(
+        new Dish("port", false, 800, Type.MEAT),
+
+        new Dish("beef", false, 700, Type.MEAT),
+        new Dish("chicken", false, 400, Type.MEAT),
+        new Dish("french fries", true, 530, Type.OTHER),
+        new Dish("rice", true, 350, Type.OTHER),
+        new Dish("season fruit", true, 120, Type.OTHER),
+        new Dish("pizza", true, 550, Type.OTHER),
+        new Dish("prawns", false, 300, Type.FISH),
+        new Dish("salmon", false, 450, Type.FISH)
+    );
+  }
+}
