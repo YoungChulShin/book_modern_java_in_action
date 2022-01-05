9e794f31bc28b28f2a9b06c2d79aa3dfda0db8a2
YoungChulShin <go1323@gmail.com>
2022-01-05 23:28:59 +0900

    그룹화 코드

--- modern-java/src/main/java/modern/java/ch6/Playground.java
+++ modern-java/src/main/java/modern/java/ch6/Playground.java
@@ -1,9 +1,20 @@
 package modern.java.ch6;
 
+import static java.util.Comparator.comparingInt;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.filtering;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.toList;
+
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import modern.java.ch6.Dish.Type;
 
 public class Playground {
 
@@ -17,7 +28,7 @@ public class Playground {
   public void testMax() {
     List<Dish> menu = Ch6Helper.getMenu();
 
-    menu.stream().max(Comparator.comparingInt(Dish::getCalories));
+    menu.stream().max(comparingInt(Dish::getCalories));
   }
 
   public void testSumInt() {
@@ -48,4 +59,79 @@ public class Playground {
     String collect = menu.stream().map(Dish::getName).collect(Collectors.joining(", "));
     System.out.println(collect);
   }
+
+  public void testGrouping() {
+    List<Dish> menu = Ch6Helper.getMenu();
+
+    Map<Type, List<Dish>> collect = menu.stream()
+        .collect(groupingBy(Dish::getType));
+  }
+
+  public void testGrouping2() {
+    List<Dish> menu = Ch6Helper.getMenu();
+
+    Map<CaloricLevel, List<Dish>> collect = menu.stream()
+        .collect(groupingBy(dish -> {
+          if (dish.getCalories() <= 400)
+            return CaloricLevel.DIET;
+          else if (dish.getCalories() <= 700)
+            return CaloricLevel.NORMAL;
+          else
+            return CaloricLevel.FAT;
+        }));
+  }
+
+  public void testGrouping3() {
+    List<Dish> menu = Ch6Helper.getMenu();
+
+    Map<Type, List<Dish>> collect = menu.stream()
+        .collect(groupingBy(
+            Dish::getType,
+            filtering(x -> x.getCalories() > 500, toList())
+        ));
+  }
+
+  public void testTwoDepthGrouping() {
+    List<Dish> menu = Ch6Helper.getMenu();
+
+    Map<Type, Map<CaloricLevel, List<Dish>>> collect = menu.stream()
+        .collect(
+            groupingBy(
+                Dish::getType,
+                groupingBy(dish -> {
+                  if (dish.getCalories() <= 400)
+                    return CaloricLevel.DIET;
+                  else if (dish.getCalories() <= 700)
+                    return CaloricLevel.NORMAL;
+                  else
+                    return CaloricLevel.FAT;
+                })
+            )
+        );
+  }
+
+  // 타입별 제일 칼로리 높은 음식 계산
+  // Map<Type, Dish>
+  public void testGrouping4() {
+    List<Dish> menu = Ch6Helper.getMenu();
+
+    Map<Type, List<Dish>> collect = menu.stream()
+        .collect(groupingBy(x -> x.getType()));
+
+    Map<Type, Optional<Dish>> collect1 = menu.stream()
+        .collect(groupingBy(Dish::getType,
+            maxBy(comparingInt(Dish::getCalories))));
+
+    Map<Type, Dish> collect2 = menu.stream()
+        .collect(groupingBy(Dish::getType,
+            collectingAndThen(
+                maxBy(comparingInt(Dish::getCalories)),
+                Optional::get)));
+
+    menu.stream()
+        .collect(Collectors.toMap(Dish::getType, Function.identity(),
+            BinaryOperator.maxBy(comparingInt(Dish::getCalories))));
+  }
+
+  public enum CaloricLevel { DIET, NORMAL, FAT }
 }
